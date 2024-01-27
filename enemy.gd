47d201c8afae199131b7d6b60e9fc9bfcc683299
Stefan Kupresak <stefan_vg@hotmail.com>
2024-01-27 13:50:53 +0100

    add slime chase

--- enemy.gd
+++ enemy.gd
@@ -16,16 +16,18 @@ func _ready():
 	attack.start()
 
 
-enum Attacks { LONG_STOMP, SPEW }
-enum State { JUMP, LANDING, IDLE }
+enum Attacks { LONG_STOMP, CHASE, SPEW }
+enum State { JUMP, LANDING, IDLE, CHASE }
 var state = State.IDLE
 
-const attack_opts = [Attacks.LONG_STOMP, Attacks.SPEW]
+const attack_opts = [Attacks.LONG_STOMP, Attacks.CHASE]
+var current_attack = null
 
 @export var landing_vel = 100
 @export var jumping_vel = -400
+@export var chase_speed = 250
 
-func _physics_process(_delta):
+func _physics_process(delta):
 	var current_target_pos = null
 	if current_target != null:
 		current_target_pos = current_target.global_position
@@ -53,13 +55,17 @@ func _physics_process(_delta):
 			
 			move_and_slide()
 	
+	if current_target_pos and state == State.CHASE:
+		global_position = global_position.move_toward(current_target_pos, 
+			delta * chase_speed)
+	
 	if current_target_pos and state == State.IDLE:
 		state = State.JUMP
 		velocity.y = jumping_vel
 
 func _on_timer_timeout():
-	var picked_attack = Attacks.LONG_STOMP
-	do_attack(picked_attack)
+	current_attack = attack_opts.pick_random()
+	do_attack(current_attack)
 	
 func do_attack(atk_type):
 	match atk_type:
@@ -71,3 +77,13 @@ func do_attack(atk_type):
 			inst.global_position = player.global_position
 			current_target = inst
 			get_tree().root.add_child(inst)
+		Attacks.CHASE:
+			$Chase.start()
+			state = State.CHASE
+			current_target = player
+
+
+func _on_chase_timeout():
+	attack.start()
+	current_target = null
+	state = State.IDLE
